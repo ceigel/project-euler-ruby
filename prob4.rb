f87486b560d1f9d6c3e9994ef25f589b67f4723a
Cristian Eigel <cristian.eigel@icloud.com>
2015-07-16 22:52:57 +0200

    Add another version for prob4

--- prob4.rb
+++ prob4.rb
@@ -1,4 +1,5 @@
 require 'benchmark'
+
 def palindrome?(num)
   str = num.to_s
   str == str.reverse
@@ -10,14 +11,24 @@ def biggest_palindrome(factor_length)
   min.upto(max).flat_map{|n| n.upto(max).map{|p| [n, p, n * p]}}.select{|a,b,n| palindrome?(n)}.max_by{|a,b,n| n}
 end
 
+def biggest_palindrome2(factor_length)
+  max = (10 ** factor_length) - 1
+  min = (10 ** (factor_length - 1)) - 1
+  max.downto(min).lazy.flat_map{|n| max.downto(n).map{|p| [n, p, n * p]}}.select{|a,b,n| palindrome?(n)}.first(10).max_by{|a,b,n| n}
+end
+
 def main(factor_length)
   palin = 0
   Benchmark.bm do |x|
-    x.report do
+    x.report('first') do
       palin = biggest_palindrome(factor_length)
+    end
+    x.report('second') do
+      palin2 = biggest_palindrome(factor_length)
     end
   end
   puts "biggest palindrome = #{palin[0]} * #{palin[1]} = #{palin[2]}"
+  puts "biggest palindrome = #{palin2[0]} * #{palin2[1]} = #{palin2[2]}"
 end
 
 if __FILE__ == $PROGRAM_NAME
